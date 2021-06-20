ae7a4b8796526d23fd541a6df7fb0b79a61d24f7
Jean Luis Urena <eljean@live.com>
2021-06-20 14:01:16 -0400

    --wip-- [skip ci]

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV === 'development';
+const mode = isDev ? 'development' : 'production';
 
 function styleLoader(isModule) {
   return [
@@ -29,26 +31,32 @@ function styleLoader(isModule) {
   ];
 }
 
-module.exports = {
-  mode:    process.env.NODE_ENV || 'development',
+const config = {
+  mode,
   context: __dirname,
+  target:  'web',
   stats:   {
     preset:      'minimal',
     performance: true
   },
+  devServer: {
+    hot:         true,
+    open:        true,
+    contentBase: path.join(__dirname, 'public')
+  },
   entry:  path.resolve(__dirname, './src/index.jsx'),
   output: {
-    path:     __dirname,
-    filename: 'public/assets/[contenthash:8].js'
+    path:     path.resolve(__dirname, 'public'),
+    filename: isDev ? 'assets/[name].js' : 'assets/[contenthash:8].js'
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename:      isDev ? '[name].css' : 'public/assets/[contenthash:8].css',
-      chunkFilename: isDev ? '[id].css' : 'public/assets[id][contenthash:8].css'
+      filename:      isDev ? 'assets/[name]' : 'assets/[contenthash:8].css',
+      chunkFilename: isDev ? 'assets/[name]' : 'assets/[id][contenthash:8].css'
     }),
     new HtmlWebpackPlugin({
       title:    'JLU',
-      filename: 'public/index.html'
+      filename: 'index.html'
     })
   ],
   module: {
@@ -74,7 +82,7 @@ module.exports = {
             loader:  'url-loader',
             options: {
               limit: 10000,
-              name:  'public/assets/[contenthash:8].[ext]'
+              name:  'assets/[contenthash:8].[ext]'
             },
           },
         ],
@@ -90,3 +98,9 @@ module.exports = {
     extensions: ['.js', '.jsx']
   }
 };
+
+if (isDev) {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+}
+
+module.exports = config;
