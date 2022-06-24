d63eee23483b5954367409c50c45faf8c9bcfae3
Jean Luis Urena <eljean@live.com>
2022-06-24 08:07:56 -0400

    LinkedIn badge working

--- src/js/components/tabs/about-me/about-me.jsx
+++ src/js/components/tabs/about-me/about-me.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCar,
@@ -18,6 +18,8 @@ import Header from '../../header';
 import styles from './about-me.module.scss';
 import '../../../../../node_modules/react-image-gallery/styles/scss/image-gallery.scss';
 import HikingImage from '../../../../images/jean/carousel/hiking.jpeg';
+import useScript from '../../../lib/useScript';
+import linkedinBadgeScript from '../../../../vendor/linkedin-badge.min';
 
 const IMAGES = [
   {
@@ -39,91 +41,95 @@ const HOBBIES = [
   { icon: faUtensils, text: 'Eating Out' },
 ];
 
-const AboutMe = () => (
-  <div className={styles.wrapper}>
-    <Header headerLevel="1" icon={faUser} headerText="About" headerStrongText="Me" fontSize="1.5rem" />
-    <div className={styles.container}>
-      <Header headerLevel="2" headerText="Hey! Nice to meet you, I'm" headerStrongText="Jean" fontSize="18px" />
-      <div className={styles.aboutMeWrapper}>
-        <div>
-          <div className={styles.aboutMeSubSection}>
-            <p>
-              <b>Software Engineer</b>
-              ,
-              {' '}
-              <b>Leader</b>
-              ,
-              {' '}
-              <b>Student</b>
-              ,
-              {' '}
-              <b>Teacher</b>
-              {' '}
-              are some adjectives that can be used to describe me.
-              At the end of the day however, I am a simple tech nerd &nbsp;
-              <FontAwesomeIcon icon={faGlasses} size="1x" />
-              &nbsp; that enjoys programming, learning new technologies, experimenting with tech and
-              teaching others all I know!
-            </p>
-          </div>
-          <div className={styles.aboutMeSubSection}>
-            <Header headerLevel="2" headerText="Hobbies & Interests" fontSize="18px" />
-            <p>
-              Of course, I have many other
-              {' '}
-              <b>hobbies</b>
-              {' '}
-              and
-              {' '}
-              <b>interests</b>
-              . Most notably though I&apos;m really into
-            </p>
-            <div className={styles.aboutMeHobbiesSection}>
-              { HOBBIES.map(i => (
-                <div className={styles.hobby} key={i.text}>
-                  <FontAwesomeIcon icon={i.icon} size="lg" />
-                  {i.text}
-                </div>
-              )) }
+const AboutMe = () => {
+  useScript(linkedinBadgeScript, { async: true, defer: true });
+  return (
+    <div className={styles.wrapper}>
+      <Header headerLevel="1" icon={faUser} headerText="About" headerStrongText="Me" fontSize="1.5rem" />
+      <div className={styles.container}>
+        <Header headerLevel="2" headerText="Hey! Nice to meet you, I'm" headerStrongText="Jean" fontSize="18px" />
+        <div className={styles.aboutMeWrapper}>
+          <div>
+            <div className={styles.aboutMeSubSection}>
+              <p>
+                <b>Software Engineer</b>
+                ,
+                {' '}
+                <b>Leader</b>
+                ,
+                {' '}
+                <b>Student</b>
+                ,
+                {' '}
+                <b>Teacher</b>
+                {' '}
+                are some adjectives that can be used to describe me.
+                At the end of the day however, I am a simple tech nerd &nbsp;
+                <FontAwesomeIcon icon={faGlasses} size="1x" />
+                &nbsp; that enjoys programming, learning new technologies, experimenting with tech and
+                teaching others all I know!
+              </p>
             </div>
-          </div>
-          <div className={styles.aboutMeSubSection}>
-            <Header headerLevel="2" headerText="Interested in my professional experience?" fontSize="18px" />
-            <div className={styles.aboutMeProfessional}>
-              <div
-                className="badge-base LI-profile-badge"
-                data-locale="en_US"
-                data-size="small"
-                data-theme="dark"
-                data-type="VERTICAL"
-                data-vanity="jlurena"
-                data-version="v1"
-              />
-              <div>
-                <a className={styles.download} target="_blank" href="./resume.html">View Resume</a>
-
+            <div className={styles.aboutMeSubSection}>
+              <Header headerLevel="2" headerText="Hobbies & Interests" fontSize="18px" />
+              <p>
+                Of course, I have many other
+                {' '}
+                <b>hobbies</b>
+                {' '}
+                and
+                {' '}
+                <b>interests</b>
+                . Most notably though I&apos;m really into
+              </p>
+              <div className={styles.aboutMeHobbiesSection}>
+                { HOBBIES.map(i => (
+                  <div className={styles.hobby} key={i.text}>
+                    <FontAwesomeIcon icon={i.icon} size="lg" />
+                    {i.text}
+                  </div>
+                )) }
               </div>
             </div>
+            <div className={styles.aboutMeSubSection}>
+              <Header headerLevel="2" headerText="Interested in my professional experience?" fontSize="18px" />
+              <div className={styles.aboutMeProfessional}>
+                <div>
+                  <div
+                    className="badge-base LI-profile-badge"
+                    data-locale="en_US"
+                    data-size="medium"
+                    data-theme="dark"
+                    data-type="HORIZONTAL"
+                    data-vanity="jlurena"
+                    data-version="v1"
+                  />
+                </div>
+                <div>
+                  <a className={styles.download} target="_blank" href="./resume.html">View Resume</a>
 
+                </div>
+              </div>
+
+            </div>
           </div>
-        </div>
-        <div>
           <div>
-            <ImageGallery
-              items={IMAGES}
-              autoPlay
-              showPlayButton={false}
-              showFullscreenButton={false}
-              showBullets
-            />
+            <div>
+              <ImageGallery
+                items={IMAGES}
+                autoPlay
+                showPlayButton={false}
+                showFullscreenButton={false}
+                showBullets
+              />
+            </div>
           </div>
+
         </div>
 
       </div>
 
     </div>
-
-  </div>
-);
-
+  );
+};
 export default AboutMe;
