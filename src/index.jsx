507d0703a100fee8257743519df2509ec701f1fb
Jean Luis Urena <eljean@live.com>
2023-07-13 18:40:32 -0400

    Removed prototypes and improved service-worker

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import * as serviceWorker from './js/lib/service-worker'
 import App from './js/app'
 
 import './scss/antispam.scss'
@@ -13,4 +12,12 @@ createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 )
 
-serviceWorker.unregister()
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('public/assets/service-worker.js').then(r => {
+      console.log('SW registered: ', r)
+    }).catch(e => {
+      console.log('SW registration failed: ', e)
+    })
+  })
+}
