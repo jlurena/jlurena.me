cfd412afd4b2ee037aac179a216c5e38a9ebfd34
Jean Luis Urena <eljean@live.com>
2023-11-25 12:14:12 -0500

    Moving back to react-script

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './js/app'
+import App from './app'
 
 import './scss/antispam.scss'
 import './scss/reset.scss'
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('public/assets/service-worker.js').then(r => {
+    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/service-worker.js`).then(r => {
       console.log('SW registered: ', r)
     }).catch(e => {
       console.log('SW registration failed: ', e)
