fae728451200f78bcced032c8908d62db4cefb2f
Jean Luis Urena <eljean@live.com>
2024-01-15 19:46:10 -0500

    Fix service-worker and icon (#32)
    * Fix service-worker and icon
    
    * Fix service-worker

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import App from './app'
 
 import './scss/antispam.scss'
@@ -12,12 +13,4 @@ createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 )
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/service-worker.js`).then(r => {
-      console.log('SW registered: ', r)
-    }).catch(e => {
-      console.log('SW registration failed: ', e)
-    })
-  })
-}
+serviceWorkerRegistration.register()
